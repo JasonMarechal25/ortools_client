75f3e35e5f2e098623a25b190c764e601f523ae5
Jason Marechal <jason.marechal+github@gmail.com>
2024-09-13 14:33:22 +0200

    Pin Abseil

--- ports/or-tools/portfile.cmake
+++ ports/or-tools/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO "google/or-tools"
-        REF "5a3253cb0f50f561b8507e3f1f6577b7829dc7c1" #stable 2.11
-        SHA512 3a2f7d207260846741d496d18f7d015d90ff3c7be565690c641e9b6c4fa98493847b87b340678275b90f71dd099fd89982709e8f8c1ba83d9222f97c0cd62adb
+        REF "b39974ba209d9f5215bdc363bb8ad9625443d618" #stable 2.11
+        SHA512 183440e6a2d821a643664bd8ca5e72a0c75adfa5cab76fa2647a3b04f0006ac86761147ae1a7e72edb6c90b626c11b3bc5ea3d5135aa654e3be7b6028ded08a2
         HEAD_REF master
 )
 
@@ -10,7 +10,7 @@ vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
         OPTIONS
         -DBUILD_SHARED_LIBS=OFF
-        -DUSE_SCIP=ON
+        -DUSE_SCIP=O
         -DUSE_GLPK=ON
         -DUSE_GLOP=OFF
         -DUSE_BOP=OFF
